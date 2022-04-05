fbee382d2d5ca393a1c507a41925d850899c1092
Dennis Sperber <dennis@rebelit.nl>
2022-04-05 22:37:20 +0200

    Added store stuff

--- kweeter-frontend/src/store/index.js
+++ kweeter-frontend/src/store/index.js
@@ -5,12 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tweets: {}
   },
   getters: {
+    tweets(state) {
+      return state.tweets
+  },
   },
   mutations: {
+    setTweets(state, tweets) {
+      state.tweets = tweets;
+  }
   },
   actions: {
+    getAllTweets(context) {
+      return axios
+          .get("http://tweet_api:51603/tweet/get")
+          .then((response) => {
+              context.commit("setTweets", response.data);
+          })
+          .catch(error => {
+              throw new Error(error)
+          });
+  },
   },
   modules: {
   }
