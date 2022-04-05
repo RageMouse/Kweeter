3632f5524f9e1056cbafd040ad35d3771b3813cd
Dennis Sperber <dennis@rebelit.nl>
2022-04-05 21:01:49 +0200

    Added database

--- kweeter-backend/TweetAPI/TweetAPI/Program.cs
+++ kweeter-backend/TweetAPI/TweetAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TweetAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TweetServiceContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TweetDatabase"));
+});
 
 var app = builder.Build();
 
