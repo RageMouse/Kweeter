f7283c15171e05e2075c2100de6586090d71bc92
Dennis Sperber <dennis@rebelit.nl>
2022-04-14 02:33:32 +0200

    Added a little something something

--- kweeter-frontend/src/views/AboutView.vue
+++ kweeter-frontend/src/views/AboutView.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-slide-y-transition mode="out-in">
+      <v-layout row wrap align-center>
+        <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg4 offset-lg4>
+          <v-card style="margin-bottom: 200px">
+            <div class="pa-3">
+              <v-img
+                alt="Vuetify Logo"
+                class="shrink mr-2"
+                contain
+                src="../assets/logo.png"
+                transition="scale-transition"
+                width="40"
+              />
+            </div>
+            <hr />
+            <div style="text-align:center">
+              <video
+                width="320"
+                height="320"
+                playsinline
+                controls
+                autoplay
+                loop
+                poster="../assets/logo.png"
+              >
+                <source
+                  :src="require('../assets/aboutme.mp4')"
+                  type="video/mp4"
+                />
+              </video>
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
