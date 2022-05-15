d28dbf695ba5813960ff1ac5a7a32f9492797b5a
Dennis Sperber <d.sperber@student.fontys.nl>
2022-05-15 14:20:32 +0200

    Added everything except database

---
+++ kweeter-backend/FollowerAPI/Data/DbServiceContext.cs
@@ -0,0 +1,20 @@
+﻿using FollowerAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FollowerAPI.Data
+{
+    public class DbServiceContext : DbContext
+    {
+        public DbServiceContext(DbContextOptions<DbServiceContext> options) : base(options)
+        {
+            // Creates the database !! Just for DEMO !! in production code you have to handle it differently!  
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Follow> Follow { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
