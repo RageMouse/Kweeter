2d98a36c9b8c1d2d2db8473de1d2f1bd18041f37
DSperber <dennissperber@live.nl>
2022-06-02 11:19:01 +0200

    Added e2e test and load test.

--- kweeter-frontend/tests/e2e/specs/test.js
+++ kweeter-frontend/tests/e2e/specs/test.js
@@ -1,8 +1,14 @@
 // https://docs.cypress.io/api/table-of-contents
+/// <reference types="Cypress" />
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.visit('https://kweeter.vercel.app/')
+    cy.contains('h3', 'Welcome')
+  }),
+
+  it('Login to Kweeter', () => {
+    cy.visit('https://kweeter.vercel.app/')
+    cy.get('button').contains('Login').click()
   })
 })
